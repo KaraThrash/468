466af81711abaf4e0ed71f5851b107dd14df9d7a
CSE4568 <dvdonato@buffalo.edu>
2019-11-03 15:07:19 -0800

    proj3

--- src/ros_pa3/scripts/finalbotpa2.py
+++ src/ros_pa3/scripts/finalbotpa2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 import math
+import rosbag
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Point
 from std_msgs.msg import String
@@ -14,6 +15,10 @@ import tf.transformations as transform
 
 
 def astartest(data):
+    bag = rosbag.Bag('test.bag')
+    for topic, msg, t in bag.read_messages(topics=['chatter', 'numbers']):
+        print msg
+    bag.close()
     print(data)
 
 
@@ -25,11 +30,23 @@ def astartoaster():
 
     # rospy.set_param('goalx', 11.0)
     # rospy.set_param('goaly', 1.0)
+    bag = rosbag.Bag('grid.bag')
+    print(bag)
+    for topic, msg, t in bag.read_messages(topics=[ 'Observations']):
+        print("Observations: ", msg.timeTag, " ", msg.tagNum," ", msg.range," ", msg.bearing.x," ", msg.bearing.y," ", msg.bearing.z," ", msg.bearing.w)
+    for topic, msg, t in bag.read_messages(topics=[ 'Movements']):
+        # print(msg)
+        print("Movements1: ", msg.timeTag, " ", round(msg.rotation1.x,2)," ", round(msg.rotation1.y,2)," ", round(msg.rotation1.z,2)," ", round(msg.rotation1.w,2))
+        print(" translation:", round(msg.translation,2))
+        print("Movements2: ",round(msg.rotation2.x,2)," ", round(msg.rotation2.y,2)," ", round(msg.rotation2.z,2)," ", round(msg.rotation2.w,2)," ")
+        # print("obs: ", msg.timeTag, " ", msg.tagNum," ", msg.range," ", msg.bearing.x," ", msg.bearing.y," ", msg.bearing.z," ", msg.bearing.w)
+
 
     while not rospy.is_shutdown():
-        rospy.loginfo('my toast')
+        # rospy.loginfo('my toast')
         rospy.Subscriber('line_list', Marker, astartest)
 
+        bag.close()
         rate.sleep()
 
 
